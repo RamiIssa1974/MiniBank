3b201f79cacd2644b3e9c4e1af6624fd7217606c
Rami Issa <ram_fad@yahoo.com>
2025-08-21 15:12:56 +0300

    [api] scaffold: Web API + health endpoint, remove template

---
+++ MiniBank.Solution/MiniBank.Api/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniBank.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() =>
+        Ok(new { status = "OK", timeUtc = DateTime.UtcNow });
+}
